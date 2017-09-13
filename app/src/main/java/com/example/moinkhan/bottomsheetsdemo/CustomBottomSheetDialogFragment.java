b5ccd9abfb488dff9fb21386be9d51d5e58328d9
Moin Khan <moin9997594451@gmail.com>
2017-09-13 13:53:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/moinkhan/bottomsheetsdemo/CustomBottomSheetDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.moinkhan.bottomsheetsdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by moin khan on 9/10/2017.
+ */
+
+public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_dialog_bottom_sheet,container,false);
+        return v;
+    }
+}
